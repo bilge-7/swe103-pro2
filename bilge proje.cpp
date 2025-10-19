071f98d1ff46276d5c06e8cd605c6a353d29106a
bilge <bilgearslantas07@gmail.com>
2025-10-19 11:40:41 +0300

    Initial commit: Hello SWE 103

--- bilge proje.cpp
+++ bilge proje.cpp
@@ -1,3 +1,5 @@
-=include ciostream> using namespace std; 
-int main() { cout « "Hello, SWE 103!" « endl; return 0; 
-
+#include <iostream>
+using namespace std; 
+int main() {
+  cout << "Hello, SWE 103!" << ENDL;
+}
